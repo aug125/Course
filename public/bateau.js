7ed07d940e0300e8895427d9d4efc516a5cafe4e
Augustin <plichonaugustin@yahoo.fr>
2016-01-15 13:59:09 +0100

    exporter classe bateau dans un autre fichier

---
+++ public/bateau.js
@@ -0,0 +1,41 @@
+function Bateau(id,x,y,j,s) {
+  
+	this.id = id;
+	this.x_dep = x;
+	this.y_dep = y;
+	this.x_dest = x;
+	this.y_dest = y;
+	this.joueur = j;
+	this.time_move = 0;
+	this.speed = s;
+	this.x = function()
+	{
+		console.log("dep : " + this.x_dep);
+		console.log("dest : " + this.x_dest);		
+		console.log("speed : " + this.speed);		
+		
+		distanceParcours = distance(this.x_dep, this.y_dep, this.x_dest, this.y_dest);
+		console.log("distance : " + distanceParcours);
+		
+		this.time_delta = (new Date().getTime() - this.time_move) / 1000; // en seconde.
+		if (this.time_delta * this.speed >= distanceParcours)			
+		{
+			return this.x_dest;
+			
+		}
+		else
+		{
+			return this.x_dep + (this.x_dest - this.x_dep) / distanceParcours * this.time_delta * this.speed;
+		}
+	};
+	this.y = function()
+	{
+		distanceParcours = distance(this.x_dep, this.y_dep, this.x_dest, this.y_dest);
+		time_delta = (new Date().getTime() - this.time_move) / 1000; // en seconde.
+
+		if (time_delta * this.speed >= distanceParcours)
+			return this.y_dest;
+		else
+			return this.y_dep + (this.y_dest - this.y_dep) / distanceParcours * this.time_delta * this.speed;
+	};
+}
